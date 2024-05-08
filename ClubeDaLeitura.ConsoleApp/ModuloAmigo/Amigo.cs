e52c4bec2634c6d95d850daaf8d116e38bccfaa6
Enzo <enzophi07@gmail.com>
2024-05-08 13:30:59 -0300

    módulo amigo

--- ClubeDaLeitura.ConsoleApp/ModuloAmigo/Amigo.cs
+++ ClubeDaLeitura.ConsoleApp/ModuloAmigo/Amigo.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ClubeDaLeitura.ConsoleApp.Compartilhado;
+using ClubeDaLeitura.ConsoleApp.ModuloMulta;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,49 @@ using System.Threading.Tasks;
 
 namespace ClubeDaLeitura.ConsoleApp.ModuloAmigo
 {
-    internal class Amigo
+    internal class Amigo : EntidadeBase
     {
+        public string Nome { get; set; }
+        public string NomeResponsavel { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+        
+
+        public Amigo(string nome, string nomeresponsavel, string telefone, string endereco)
+        {
+            Nome = nome;
+            NomeResponsavel = nomeresponsavel;
+            Telefone = telefone;
+            Endereco = endereco;
+        }
+
+        public override ArrayList Validar()
+        {
+            ArrayList erros = new ArrayList();
+
+            if (string.IsNullOrEmpty(Nome.Trim()))
+                erros.Add("O campo \"nome\" é obrigatório");
+
+            if (string.IsNullOrEmpty(NomeResponsavel.Trim()))
+                erros.Add("O campo \"nome do responsável\" é obrigatório");
+
+            if (string.IsNullOrEmpty(Telefone.Trim()))
+                erros.Add("O campo \"Telefone\" é obrigatório");
+
+            if (string.IsNullOrEmpty(Endereco.Trim()))
+                erros.Add("O campo \"Endereço\" é obrigatório");
+
+            return erros;
+        }
+
+        public override void AtualizarRegistro(EntidadeBase novoRegistro)
+        {
+            Amigo novasInformacoes = (Amigo)novoRegistro;
+
+            this.Nome = novasInformacoes.Nome;
+            this.NomeResponsavel = novasInformacoes.NomeResponsavel;
+            this.Telefone = novasInformacoes.Telefone;
+            this.Endereco = novasInformacoes.Endereco;
+        }
     }
 }
--- ClubeDaLeitura.ConsoleApp/ModuloAmigo/TelaAmigo.cs
+++ ClubeDaLeitura.ConsoleApp/ModuloAmigo/TelaAmigo.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ClubeDaLeitura.ConsoleApp.Compartilhado;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,59 @@ using System.Threading.Tasks;
 
 namespace ClubeDaLeitura.ConsoleApp.ModuloAmigo
 {
-    internal class TelaAmigo
+    internal class TelaAmigo : TelaBase
     {
+        public override void VisualizarRegistros(bool exibirTitulo)
+        {
+            if (exibirTitulo)
+            {
+                ApresentarCabecalho();
+
+                Console.WriteLine("Visualizando Amigos...");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(
+                "{0, -10} | {1, -20} | {2, -20} | {3,-20} | {4, -15}",
+                "Id", "Nome", "Nome do Responsável", "Telefone", "Endereço"
+            );
+
+            ArrayList amigosCadastrados = repositorio.SelecionarTodos();
+
+            foreach (Amigo amigo in amigosCadastrados)
+            {
+                if (amigo == null)
+                    continue;
+
+                Console.WriteLine(
+                    "{0, -10} | {1, -20} | {2, -20} | {3,-20} | {4, -15}",
+                    amigo.Id, amigo.Nome, amigo.NomeResponsavel, amigo.Telefone, amigo.Endereco
+                );
+            }
+
+            Console.ReadLine();
+            Console.WriteLine();
+        }
+        protected override EntidadeBase ObterRegistro()
+        {
+            Console.Write("Digite o nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite o nome do responsável: ");
+            string nomeresponsavel = Console.ReadLine();
+
+            Console.Write("Digite o telefone: ");
+            string telefone = Console.ReadLine();
+
+            Console.WriteLine("Digite o endereço: ");
+            string endereco = Console.ReadLine();
+
+            Amigo amigo = new Amigo(nome, nomeresponsavel, telefone, endereco);
+
+            return amigo;
+        }
+
+        
     }
 }
