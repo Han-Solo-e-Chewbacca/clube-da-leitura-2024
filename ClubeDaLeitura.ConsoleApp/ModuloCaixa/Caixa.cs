772b624d0ae91d11f950e231df51c6b44d9c5cbf
Enzo <enzophi07@gmail.com>
2024-05-08 14:17:32 -0300

    módulo caixa

--- ClubeDaLeitura.ConsoleApp/ModuloCaixa/Caixa.cs
+++ ClubeDaLeitura.ConsoleApp/ModuloCaixa/Caixa.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ClubeDaLeitura.ConsoleApp.Compartilhado;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,44 @@ using System.Threading.Tasks;
 
 namespace ClubeDaLeitura.ConsoleApp.ModuloCaixa
 {
-    internal class Caixa
+    internal class Caixa : EntidadeBase
     {
+        public string Etiqueta { get; set; }
+        public string Cor { get; set; }
+        public int DiasEmprestados { get; set; }
+        
+
+        public Caixa(string etiqueta, string cor, int diasEmprestimo)
+        {
+            Etiqueta = etiqueta;
+            Cor = cor;
+            DiasEmprestados = diasEmprestimo;
+        }
+
+        public override ArrayList Validar()
+        {
+            ArrayList erros = new();
+
+            if (string.IsNullOrEmpty(Etiqueta.Trim()))
+                erros.Add("O campo \"etiqueta\" é obrigatório");
+
+            if (string.IsNullOrEmpty(Cor.Trim()))
+                erros.Add("O campo \"cor\" é obrigatório");
+
+            if (DiasEmprestados < 1)
+                erros.Add("O campo \"dias de emprestimo\" é obrigatório");
+
+            return erros;
+        }
+
+        public override void AtualizarRegistro(EntidadeBase novoRegistro)
+        {
+            Caixa caixa = (Caixa)novoRegistro;
+
+            this.Etiqueta = caixa.Etiqueta;
+            this.Cor = caixa.Cor;
+            this.DiasEmprestados = caixa.DiasEmprestados;
+            
+        }
     }
 }
--- ClubeDaLeitura.ConsoleApp/ModuloCaixa/TelaCaixa.cs
+++ ClubeDaLeitura.ConsoleApp/ModuloCaixa/TelaCaixa.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ClubeDaLeitura.ConsoleApp.Compartilhado;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,56 @@ using System.Threading.Tasks;
 
 namespace ClubeDaLeitura.ConsoleApp.ModuloCaixa
 {
-    internal class TelaCaixa
+    internal class TelaCaixa : TelaBase
     {
+        public override void VisualizarRegistros(bool exibirTitulo)
+        {
+            if (exibirTitulo)
+            {
+                ApresentarCabecalho();
+
+                Console.WriteLine("Visualizando Caixas...");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(
+                "{0, -10} | {1, -20} | {2, -10} | {3,-20} | {4,-24}",
+                "Id", "Etiqueta", "Cor", "Dias emprestados", "Número de Revistas" //colocar revistas
+            );
+
+            ArrayList caixasCadastradas = repositorio.SelecionarTodos();
+
+            foreach (Caixa caixa in caixasCadastradas)
+            {
+                if (caixa == null)
+                    continue;
+
+                Console.WriteLine(
+                    "{0, -10} | {1, -20} | {2, -10} | {3,-20} | {4, -15}",
+                    caixa.Id, caixa.Etiqueta, caixa.Cor, caixa.DiasEmprestados
+                );
+            }
+
+            Console.ReadLine();
+            Console.WriteLine();
+        }
+        protected override EntidadeBase ObterRegistro()
+        {
+            Console.Write("Digite o nome da etiqueta: ");
+            string etiqueta = Console.ReadLine();
+
+            Console.Write("Digite a cor da caixa: ");
+            string cor = Console.ReadLine();
+
+            Console.Write("Digite quantos dias de empréstimo terá: ");
+            int dias = int.Parse(Console.ReadLine());
+
+            Caixa caixa = new Caixa(etiqueta, cor, dias);
+
+            return caixa;
+        }
+
+       
     }
 }
